c5665c630a6813908dceb5c6d0123ee6cedb5bcc
Eric Ortega <24722023+ejortega@users.noreply.github.com>
2025-03-24 21:32:54 -0500

    Update host build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull ejortega/morse-lib .
+docker pull ejortega/morse-lib
 container_id=$(docker create ejortega/morse-lib:latest)
 docker cp $container_id:/app/target/riscv64gc-unknown-linux-musl/release/libmorse.so libmorse-riscv64.so
 docker cp $container_id:/app/target/aarch64-unknown-linux-gnu/release/libmorse.so libmorse-aarch64.so
